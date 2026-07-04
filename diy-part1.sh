a10c82b6860ddce8d54c354227b8a93d5b0c0d15
aliass532 <56620619+aliass532@users.noreply.github.com>
2026-07-04 18:13:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@ sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generat
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
 echo 'src-git mt5700webui https://github.com/aliass532/mt5700webui-openwrt-server.git;main' >> feeds.conf.default
 
-echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git^4c2967c6511274d82a0dd099cf91d361c311fb59;main' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git^2be2586fe72f07024326f7e590f7c6de99aaf469;main' >> feeds.conf.default
 
 #echo 'CONFIG_TCP_CONG_BBR=y' >> target/linux/ky/riscv64/config-6.6
 #echo 'CONFIG_DEFAULT_TCP_CONG="bbr"' >> target/linux/ky/riscv64/config-6.6
